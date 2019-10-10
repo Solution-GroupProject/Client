dbc1fba1c4042da1e80f27e7500b24bbf1bcb99e
arnold_therigan <arnoldtherigan15@gmail.com>
2019-10-10 19:16:46 +0700

    homepage

---
+++ main.js
@@ -0,0 +1,18 @@
+let app = new Vue ({
+    el:'#app',
+    data:{
+        isHome:true
+    },
+    methods: {
+       
+    },
+    watch:{
+       
+    },
+    mounted() {
+        // FILE JAVASCRIPT FOR LOGIN
+       
+        // END OF FILE JAVASCRIPT
+
+    }
+})
